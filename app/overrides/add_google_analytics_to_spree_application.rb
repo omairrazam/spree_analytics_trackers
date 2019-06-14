5db668f43c7f937e9edd02537231d07a026fbe25
yaseen iqbal <p136011@nu.edu.pk>
2019-06-14 20:50:26 +0500

    change placement tag for root analytic script

--- app/overrides/add_google_analytics_to_spree_application.rb
+++ app/overrides/add_google_analytics_to_spree_application.rb
@@ -3,7 +3,7 @@ if Gem.loaded_specs['spree_core'].version >= Gem::Version.create('3.5.0.alpha')
   Deface::Override.new(
     virtual_path: "spree/#{lang.present? ? lang.to_s : DEFAULT_VIEW_LANG}/layouts/spree_application",
     name: 'add_google_analytics_to_spree_application',
-    insert_top: "[data-hook='body']",
+    insert_top: "[data-hook='inside_head']",
     partial: 'spree/shared/google_analytics',
     original: 'cfa30a2831d9a41394c03229cd28b3c7eee69585'
   )
