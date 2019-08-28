957e279762777a0d8f152e6b313f8d2f22a0d72f
Umair <omsolutionpk@gmail.com>
2019-08-28 12:33:38 +0500

    partial path fixed.

--- app/overrides/add_google_analytics_to_spree_application.rb
+++ app/overrides/add_google_analytics_to_spree_application.rb
@@ -1,7 +1,7 @@
 if Gem.loaded_specs['spree_core'].version >= Gem::Version.create('3.5.0.alpha')
   lang = I18n.locale
   Deface::Override.new(
-    virtual_path: "spree/#{lang.present? ? lang.to_s : DEFAULT_VIEW_LANG}/layouts/spree_application",
+    virtual_path: "spree/layouts/spree_application",
     name: 'add_google_analytics_to_spree_application',
     insert_top: "[data-hook='inside_head']",
     partial: 'spree/shared/google_analytics',
